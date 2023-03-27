b36c03a64850896aca428a756d5faa218d5032e9
Olga Suvorova <olga.suv.ya@yandex.ru>
2023-03-27 12:34:15 +0300

    Полностью решены две задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -20,61 +20,20 @@ console.log(solveEquation(1, 2, 10));
 
 
 "use strict";
-function calculateMortgage() {
-  let percent = window.percent.value;
-  let contribution = window.contribution.value;
-  let amount = window.amount.value;
-  let countMonths = window.countMonths.value;
-
-  if (isNaN(percent, contribution, amount, countMonths)) {
-    let result = calculateTotalMortgage(
-      percent,
-      contribution,
-      amount,
-      countMonths
-    )
-    let span = window.mortgageResult;
-    span.textContent = result;
-  } else 
-    return false;
-}
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  // Контроль корректность введенных данных.
-  if (percent > 0) {
-    percent = +percent;
-  } else if (percent != 0 || percent == 0) {
-    return `“Параметр "Процентная ставка" содержит неправильное значение”.`;
-  }
-
-  if (contribution >= 0) {
-    contribution = +contribution;
-  } else if (contribution != 0) {
-    return `“Параметр "Первоночальный взнос" содержит неправильное значение”.`;
-  }
-
-  if (amount >= 0) {
-    amount = +amount;
-  } else if (amount != 0) {
-    return `“Параметр "Общая сумма" содержит неправильное значение”.`;
-  }
-
-  // Расчет количества месяцев, на которые оформляется ипотека.
-  let currentDate = new Date(); // Дата с какого числа берется ипотеки
-  countMonths = new Date(window.countMonths.value); //Дата до какого числа берется ипотека
-  let payPeriod =0 - (currentDate.getFullYear() - countMonths.getFullYear()) * 12 - (currentDate.getMonth() - countMonths.getMonth());
-  countMonths = payPeriod; // Количество месяцев, на которые оформляется ипотека.
-  let returnAmount = amount - contribution; // Тело кредита, которое необходимо вернуть банку.
-  percent = percent / 100 / 12; // Преобразование процентной ставки из годовой в месячную.
-  let monthlyPay = amount * (percent + percent / ((1 + percent) ** countMonths - 1)); // Ежемесячный платеж.
-  let totalAmount = monthlyPay * countMonths; // Сумма, которую придется заплатить клиенту.
-  console.log(totalAmount.toFixed(2));
-  return totalAmount.toFixed(2); //  Возврат результата в функцию
+  if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount)) {
+   percent = percent / 100 / 12; // Преобразование процентной ставки из годовой в месячную.
+   let returnAmount = amount - contribution; // Тело кредита, которое необходимо вернуть банку.
+   let monthlyPay = returnAmount * (percent + percent / ((1 + percent) ** countMonths - 1)); // Ежемесячный платеж.
+   let totalAmount = Number(monthlyPay * countMonths); // Сумма, которую необходимо вернуть.   
+   return Number(totalAmount.toFixed(2));
+ }
+   return false;
 }
 
-console.log(calculateTotalMortgage(0, 0, 10000, 36));
-console.log(calculateTotalMortgage(15, -5, 10000, 36));
-console.log(calculateTotalMortgage(15, 0, -8, 36));
+console.log(calculateTotalMortgage("percent", 0, 10000, 36));
+console.log(calculateTotalMortgage(15, "contribution", 10000, 36));
+console.log(calculateTotalMortgage(15, 0, "amount", 36));
 
 console.log(calculateTotalMortgage(10, 0, 50000, 12));
 console.log(calculateTotalMortgage(10, 1000, 50000, 12));
