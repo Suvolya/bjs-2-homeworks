ec0276c110d8ae689500745286b4cc4693fdda3e
Olga Suvorova <olga.suv.ya@yandex.ru>
2023-04-07 15:01:46 +0300

    Полностью решено две задачи

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    if (arr1.length == arr2.length && arr1.every((n, i) => arr2[i] == n)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
 
 function getUsersNamesInAgeRange(users, gender) {
+  const filteredUsers = users.filter(user => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const ages = filteredUsers.map(user => user.age);
+  const totalAge = ages.reduce((acc, age) => acc + age, 0);
+
   
+  const average = totalAge/filteredUsers.length;
+  return average;
 }
