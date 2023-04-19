c891b08aa073c8ca806d56cde7f894e0508ac110
Olga Suvorova <olga.suv.ya@yandex.ru>
2023-04-19 16:31:20 +0300

    Решено две задачи

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(n) {
+    let parseResult = Number.parseFloat(n);
+    if (Number.isNaN(parseResult)) throw new Error("Невалидное значение");
+    return parseResult;
+}
+
+function validateCount(n) {
+    try { 
+        return parseCount(n) 
+    } catch (error) {
+      console.log(error)
+      console.log("Перехватили ошибку")
+        return error;
+    }
+  }
+
+  class Triangle {
+    constructor (a, b, c) {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+      if ((a + b) < c || (a + c) < b || (b + c) < a) {
+        throw new Error ("Треугольник с такими сторонами не существует")
+      }
+    }
+  
+    get perimeter() {
+      return this.a + this.b + this.c;
+    }
+  
+    get area() {
+      const p = this.perimeter / 2;
+      return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+    }
+  }
+  
+  function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+          get area() {
+            return "Ошибка! Треугольник не существует";
+          },
+          get perimeter() {
+            return "Ошибка! Треугольник не существует";
+          },
+        }
+      }
+  }
+  
